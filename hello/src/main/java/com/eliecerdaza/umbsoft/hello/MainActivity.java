c9be7d1f189da295f8a3300cc509f0c56082f71c
eliecer daza <elin3t@gmail.com>
2018-04-24 14:38:35 -0500

    Add Button to Open UMB Web on Browser

--- hello/src/main/java/com/eliecerdaza/umbsoft/hello/MainActivity.java
+++ hello/src/main/java/com/eliecerdaza/umbsoft/hello/MainActivity.java
@@ -1,7 +1,10 @@
 package com.eliecerdaza.umbsoft.hello;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openUMBinBrowser(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://umbvirtual.edu.co"));
+        startActivity(browserIntent);
     }
 }
